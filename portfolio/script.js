78757e24d32b707c0043910c7f83dede30320a6f
DhruvTalwarr <156096641+DhruvTalwarr@users.noreply.github.com>
2025-08-16 17:44:49 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,7 +0,0 @@
-// Smooth Scroll
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
-    });
-});
